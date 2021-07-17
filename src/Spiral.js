559a0a6e681f08d12e8eba67d7dca65e6b531e6e
Valentin Valls <valentin.valls@esrf.fr>
2021-07-17 12:59:40 +0200

    Optimize the path using elliptical command

--- src/Spiral.js
+++ src/Spiral.js
@@ -4,6 +4,7 @@ const pointOnCircle = (center, radius, angle) => ({
     x: center + radius * Math.cos(angle),
     y: center + radius * Math.sin(angle),
 })
+
 const degTorad = deg => (Math.PI * deg) / 180
 
 function ordered(v1, p1, v2, p2) {
@@ -11,7 +12,6 @@ function ordered(v1, p1, v2, p2) {
         return [v1, p1, v2, p2]
     } else {
         return [v2, p2, v1, p1]
-
     }
 }
 
@@ -32,20 +32,31 @@ const calcPath = ({
     const startAngleRad = degTorad(startAngle)
     const endAngleRad = degTorad(startAngle + angle)
 
-	const nb = Math.ceil(percentageMax - percentageMin) * 40
+	const nb = Math.ceil(percentageMax - percentageMin) * 4
 	let forth = ""
+	let start = ""
 	let back = ""
+	let link = ""
 	for (let i = 0; i <= nb; i++) {
-		const coef = i / nb
+        const coef = i / nb
         const outerRadius = outerRadiusMin + (outerRadiusMax - outerRadiusMin) * coef
         const innerRadius = outerRadius - arcWidth
         const angleRad = startAngleRad + (endAngleRad - startAngleRad) * coef
-	    const p1 = pointOnCircle(center, outerRadius, angleRad)
-    	const p2 = pointOnCircle(center, innerRadius, angleRad)
-        forth += `${p1.x},${p1.y} `
-        back = `${p2.x},${p2.y} ` + back
+        const angleDeg = angleRad * 180 / Math.PI * coef
+        const po = pointOnCircle(center, outerRadius, angleRad)
+        const pi = pointOnCircle(center, innerRadius, angleRad)
+        if (i === 0) {
+            start = `${po.x},${po.y} `
+        } else {
+            forth += `${outerRadius},${outerRadius} ${angleDeg} 0 1 ${po.x},${po.y} `
+        }
+        if (i === nb) {
+            link = `${pi.x},${pi.y} `
+        } else {
+            back = `${innerRadius},${innerRadius} ${angleDeg} 0 0 ${pi.x},${pi.y} ` + back
+        }
 	}
-    return "M" + forth + back + "z"
+    return `M ${start}A ${forth}L ${link}A ${back}z`
 }
 
 export const Spiral = ({

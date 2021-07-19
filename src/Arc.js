16f299bd8cb1386722d67e453f7fea028917e694
Valentin Valls <valentin.valls@esrf.fr>
2021-07-20 00:38:23 +0200

    Rework Arc using Range

--- src/Arc.js
+++ src/Arc.js
@@ -1,48 +1,21 @@
 import React from 'react'
+import { Range } from './Range'
 
-const pointOnCircle = (center, radius, angle) => ({
-    x: center + radius * Math.cos(angle),
-    y: center + radius * Math.sin(angle),
-})
-const degTorad = deg => (Math.PI * deg) / 180
-
-const calcPath = ({
-    percentage,
-    angleOffset,
-    angleRange,
-    arcWidth,
-    radius: outerRadius,
-    center,
-}) => {
-    const angle = angleRange * percentage
-    const startAngle = angleOffset - 90
-    const innerRadius = outerRadius - arcWidth
-    const startAngleRad = degTorad(startAngle)
-    const endAngleRad = degTorad(startAngle + angle)
-    const largeArcFlag = angle < 180 ? 0 : 1
-
-    const p1 = pointOnCircle(center, outerRadius, endAngleRad)
-    const p2 = pointOnCircle(center, outerRadius, startAngleRad)
-    const p3 = pointOnCircle(center, innerRadius, startAngleRad)
-    const p4 = pointOnCircle(center, innerRadius, endAngleRad)
-
-    return `M${p1.x},${
-        p1.y
-    } A${outerRadius},${outerRadius} 0 ${largeArcFlag} 0 ${p2.x},${p2.y}L${
-        p3.x
-    },${p3.y} A${innerRadius},${innerRadius} 0 ${largeArcFlag} 1 ${p4.x},${
-        p4.y
-    } L${p1.x},${p1.y}`
-}
-
-export const Arc = ({ color, background, ...props }) => (
+export const Arc = ({ percentage, color, background, ...props }) => (
     <g>
         {background && (
-            <path
-                d={calcPath({ ...props, percentage: 1 })}
-                style={{ fill: background }}
+            <Range
+                percentageFrom={percentage}
+                percentageTo={1}
+                color={background}
+                { ...props }
             />
         )}
-        <path d={calcPath(props)} style={{ fill: color }} />
+        <Range
+            percentageFrom={0}
+            percentageTo={percentage}
+            color={color}
+            { ...props }
+        />
     </g>
 )

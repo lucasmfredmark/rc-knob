e215857dfe370096d5c014e19d03f325eae35d3a
Valentin Valls <valentin.valls@esrf.fr>
2021-08-03 00:26:17 +0200

    Refactor pointer shape

--- src/Pointer.js
+++ src/Pointer.js
@@ -1,43 +1,59 @@
 import React from 'react'
 
+
+const PointerShape = ({ type, width, height, color, className }) => {
+	switch (type) {
+		case 'rect':
+			return (<rect
+				x={-width * 0.5}
+				width={width}
+				height={height}
+				fill={color}
+				className={className}
+			/>)
+		case 'circle':
+			return (<circle
+				r={width}
+				fill={color}
+				className={className}
+			/>)
+	}
+}
+
+
 export const Pointer = ({
-    children,
-    width,
-    height = width,
-    angleOffset,
-    angleRange,
-    percentage,
-    radius,
-    center,
-    type,
-    color,
-    className,
+	children,
+	width,
+	height = width,
+	angleOffset,
+	angleRange,
+	percentage,
+	radius,
+	center,
+	type,
+	color,
+	className,
 }) => (
-    <g
-        transform={`
+	<g
+		transform={`
         rotate(${angleOffset + angleRange * percentage} ${center} ${center})
         translate( ${center} ${center - radius - height})
         `}
-    >
-        {children &&
-            React.Children.map(children, child =>
-                React.cloneElement(child, {
-                    width,
-                    height,
-                    percentage,
-                })
-            )}
-        {type === 'rect' && (
-            <rect
-                x={-width * 0.5}
-                width={width}
-                height={height}
-                fill={color}
-                className={className}
-            />
-        )}
-        {type === 'circle' && (
-            <circle r={width} fill={color} className={className} />
-        )}
-    </g>
+	>
+		{children &&
+			React.Children.map(children, child =>
+				React.cloneElement(child, {
+					width,
+					height,
+					percentage,
+				})
+			)}
+		{type && (<PointerShape
+			type={type}
+			width={width}
+			height={height}
+			color={color}
+			className={className}
+		/>)}
+	</g>
 )
